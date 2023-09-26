b5b2490fdbee966b14ac7bde5a9b620820d60a11
Your Name <you@example.com>
2023-09-26 16:52:29 -0300

    funcion temporal

--- apps/core/views.py
+++ apps/core/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views.generic import ListView, CreateView
 from django.urls import reverse_lazy
 
+import os
+from re import findall
 
 from .models import ListXlsx
 # Create your views here.
@@ -26,3 +28,16 @@ class ListXlsx(ListView):
     model = ListXlsx
     template_name = 'core/index.html'
     context_object_name = 'listXlsx'
+
+#funcion temporal para registrar listas xlsx en la db
+def temp_create_listXlsx(request):
+    carpeta = 'Y:\\Lista de Precio\\'
+    if os.path.exists(carpeta):
+    # Obtener una lista de todos los archivos en la carpeta
+    archivos = os.listdir(carpeta)
+
+    # Imprimir el nombre de cada archivo
+    for archivo in archivos:
+        result = findall(".xlsx$", archivo)
+        if result:
+            print(archivo)
