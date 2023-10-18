ced931b0790f143994c9c0143e84ba36e6f42c78
mrkein <luquealanbri@gmail.com>
2023-10-18 12:21:36 -0300

    buscador en la vista selectList, descripciones detalladas al dejar el mouse en porcentaje

--- static/js/script.js
+++ static/js/script.js
@@ -31,3 +31,85 @@ function handleCheckboxClick(checkbox) {
       }
   });
 }
+
+
+function searchTable() {
+  // Obtén el término de búsqueda
+  var input = document.getElementById('searchInput');
+  var filter = input.value.toUpperCase();
+
+  // Obtén la tabla y las filas
+  var table = document.querySelector('.table');
+  var rows = table.getElementsByTagName('tr');
+
+  // Recorre las filas y oculta las que no coincidan con el término de búsqueda
+  for (var i = 1; i < rows.length; i++) {  // Comienza en 1 para omitir la fila de encabezado
+      var cells = rows[i].getElementsByTagName('td');
+      var shouldShowRow = false;
+
+      // Verifica cada celda de la fila para el término de búsqueda
+      for (var j = 1; j < cells.length; j++) {  // Comienza en 1 para omitir la primera celda con el checkbox
+          var cellText = cells[j].textContent || cells[j].innerText;
+          if (cellText.toUpperCase().indexOf(filter) > -1) {
+              shouldShowRow = true;
+              break;
+          }
+      }
+
+      // Muestra u oculta la fila
+      if (shouldShowRow) {
+          rows[i].style.display = '';
+      } else {
+          rows[i].style.display = 'none';
+      }
+  }
+}
+
+function searchTable2() {
+  // Obtén el término de búsqueda
+  var input = document.getElementById('searchInput');
+  var filter = input.value.toUpperCase();
+  var keywords = filter.split(' ');  // Divide el término de búsqueda en palabras
+
+  // Obtén la tabla y las filas
+  var table = document.querySelector('.table');
+  var rows = table.getElementsByTagName('tr');
+
+  // Recorre las filas y oculta las que no coincidan con las palabras de búsqueda
+  for (var i = 1; i < rows.length; i++) {  // Comienza en 1 para omitir la fila de encabezado
+      var cells = rows[i].getElementsByTagName('td');
+      var shouldShowRow = true;  // Asume que la fila debe mostrarse inicialmente
+
+      // Verifica cada palabra de búsqueda
+      for (var k = 0; k < keywords.length; k++) {
+          var keyword = keywords[k];
+          var found = false;  // Asume que la palabra no se ha encontrado en la fila
+
+          // Verifica cada celda de la fila para la palabra de búsqueda
+          for (var j = 1; j < cells.length; j++) {  // Comienza en 1 para omitir la primera celda con el checkbox
+              var cellText = cells[j].textContent || cells[j].innerText;
+              if (cellText.toUpperCase().indexOf(keyword) > -1) {
+                  found = true;
+                  break;
+              }
+          }
+
+          // Si la palabra de búsqueda no se encuentra en ninguna celda, oculta la fila
+          if (!found) {
+              shouldShowRow = false;
+              break;  // No es necesario verificar más palabras si una no se encuentra
+          }
+      }
+
+      // Muestra u oculta la fila
+      if (shouldShowRow) {
+          rows[i].style.display = '';
+      } else {
+          rows[i].style.display = 'none';
+      }
+  }
+}
+
+// Agrega un event listener para llamar a la función de búsqueda cuando se escriba en el campo de búsqueda
+//var searchInput = document.getElementById('searchInput');
+//searchInput.addEventListener('input', searchTable);
